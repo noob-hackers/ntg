b51fccf0f38fd00f1cb21660dc5f3bc8ce7fee70
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2022-11-26 08:11:46 +0530

    Create ad-cpgn.js

---
+++ ad-cpgn.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'e92fc4029fed22259ddadc684a3e9758',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://inchexplicitwindfall.com/e92fc4029fed22259ddadc684a3e9758/invoke.js"></scr' + 'ipt>');
+</script>
